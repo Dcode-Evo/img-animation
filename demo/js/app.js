2d758fc86f414278229219e336f26d5a9ff63568
alek <alekart@users.noreply.github.com>
2016-10-26 18:03:09 +0200

    add demo (serve it to work)

---
+++ demo/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name befrApp
+ * @description
+ * # befrApp
+ *
+ * Main module of the application.
+ */
+var app;
+(function (window, angular, undefined) {
+	'use strict';
+
+	app = angular.module('testApp', [
+		'imgAnimation'
+	]);
+
+})(window, window.angular);
